8039992dbc7238f5f6683c721b0c2c197e22a59c
Thomas Friebel <yaron@codefreax.com>
2013-05-04 16:00:22 +0200

    Add simple command line parsing

--- iTunesRefresh.py
+++ iTunesRefresh.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
+import appscript
+import argparse
 import datetime
+import mactypes
 import os
 import sys
-import appscript
-import mactypes
 
-verbose = False
-
-new_playlist_name = 'Import %s' % datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
+def parse_args():
+    parser = argparse.ArgumentParser(description='Refresh iTunes database after library changes on disk.')
+    parser.add_argument('-v', dest='verbosity', action='count', default=0,
+                        help='verbosity level (default: %(default)s)')
+    args = parser.parse_args()
+    return args
 
 def walk_library(lib_path):
     def is_bundle_path(path):
@@ -45,94 +49,103 @@ def get_track_data(track):
             }
     return track_data
 
-itunes_lib_path = os.environ['HOME'] + u'/Music/iTunes/iTunes Music'
+def main():
+    args = parse_args()
 
-print 'Searching for music in %s' % repr(itunes_lib_path)
-files_on_disk = walk_library(itunes_lib_path)
+    new_playlist_name = 'Import %s' % datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
 
-print '%s files found' % len(files_on_disk)
-if 0:
-    print '\t' + '\n\t'.join([repr(x) for x in files_on_disk])
+    itunes_lib_path = os.environ['HOME'] + u'/Music/iTunes/iTunes Music'
 
-print 'Connecting to iTunes'
-itunes = appscript.app('itunes')
+    print 'Searching for music in %s' % repr(itunes_lib_path)
+    files_on_disk = walk_library(itunes_lib_path)
 
-library = itunes.library_playlists()[0]
-library_tracks = library.file_tracks()
-print 'Playlist "%s" contains %s files of %s MB. This is %s of music.' % (
-        library.name(), len(library_tracks), library.size() / 1024 / 1024, library.time()
-        )
+    print '%s files found' % len(files_on_disk)
+    if 0:
+        print '\t' + '\n\t'.join([repr(x) for x in files_on_disk])
 
-print
+    print 'Connecting to iTunes'
+    itunes = appscript.app('itunes')
 
-print 'Checking iTunes track list'
+    library = itunes.library_playlists()[0]
+    library_tracks = library.file_tracks()
+    print 'Playlist "%s" contains %s files of %s MB. This is %s of music.' % (
+            library.name(), len(library_tracks), library.size() / 1024 / 1024, library.time()
+            )
 
-tracks_to_remove = []
-files_to_add = list(files_on_disk)
+    print
 
-idx_format = '%%%ss/%s' % (len(str(len(library_tracks))), len(library_tracks))
+    print 'Checking iTunes track list'
 
-for idx, track in enumerate(library_tracks):
-    if track.podcast():
-        if verbose:
-            print idx_format % (idx + 1) + ' Skipping podcast %s' % ', '.join(['%s=%s' % (k, repr(v)) for k, v in get_track_data(track).iteritems()])
-        continue
+    tracks_to_remove = []
+    files_to_add = list(files_on_disk)
 
-    if track.location() == appscript.k.missing_value:
-        message = 'item with unknown location %s' % ', '.join(['%s=%s' % (k, repr(v)) for k, v in get_track_data(track).iteritems()])
-        print idx_format % (idx + 1) + ' ' + message
-        tracks_to_remove.append((track, message))
-        continue
+    idx_format = '%%%ss/%s' % (len(str(len(library_tracks))), len(library_tracks))
 
-    path = track.location().path
-    if path in files_to_add:
-        if verbose:
-            print idx_format % (idx + 1) + ' Found    %s' % repr(path)
-        files_to_add.remove(path)
-    else:
-        message = 'track with missing file %s' % repr(path)
-        print idx_format % (idx + 1) + ' ' + message
-        tracks_to_remove.append((track, message))
+    for idx, track in enumerate(library_tracks):
+        if track.podcast():
+            if args.verbosity >= 2:
+                print idx_format % (idx + 1) + ' Skipping podcast %s' % ', '.join(['%s=%s' % (k, repr(v)) for k, v in get_track_data(track).iteritems()])
+            continue
 
-# Get list of playlists
-user_playlists = dict([(list.name(), list) for list in itunes.user_playlists()])
-try:
-    new_playlist = user_playlists[new_playlist_name]
-except KeyError:
-    new_playlist = None
+        if track.location() == appscript.k.missing_value:
+            if args.verbosity >= 1:
+                message = 'item with unknown location %s' % ', '.join(['%s=%s' % (k, repr(v)) for k, v in get_track_data(track).iteritems()])
+                print idx_format % (idx + 1) + ' ' + message
+            tracks_to_remove.append((track, message))
+            continue
 
-print
+        path = track.location().path
+        if path in files_to_add:
+            if args.verbosity >= 3:
+                print idx_format % (idx + 1) + ' Found    %s' % repr(path)
+            files_to_add.remove(path)
+        else:
+            if args.verbosity >= 1:
+                message = 'track with missing file %s' % repr(path)
+                print idx_format % (idx + 1) + ' ' + message
+            tracks_to_remove.append((track, message))
 
-# Print task list for approval
-print '%s tracks to remove' % len(tracks_to_remove)
-idx_format = '%%%ss/%s' % (len(str(len(tracks_to_remove))), str(len(tracks_to_remove)))
-for idx, (track, message) in enumerate(tracks_to_remove):
-    print idx_format % (idx + 1) + ' Remove ' + message
+    # Get list of playlists
+    user_playlists = dict([(plist.name(), plist) for plist in itunes.user_playlists()])
+    try:
+        new_playlist = user_playlists[new_playlist_name]
+    except KeyError:
+        new_playlist = None
 
-print '%s new files to add' % len(files_to_add)
-idx_format = '%%%ss/%s' % (len(str(len(files_to_add))), str(len(files_to_add)))
-for idx, path in enumerate(files_to_add):
-    print idx_format % (idx + 1) + ' Add %s' % repr(path)
+    print
 
-if len(tracks_to_remove) or len(files_to_add):
-    # Ask for permission
-    print 'Press Enter to continue or Ctrl-C to quit.'
-    sys.stdin.readline()
-
-    # Run tasks
-    print 'Removing %s tracks' % len(tracks_to_remove)
+    # Print task list for approval
+    print '%s tracks to remove' % len(tracks_to_remove)
     idx_format = '%%%ss/%s' % (len(str(len(tracks_to_remove))), str(len(tracks_to_remove)))
     for idx, (track, message) in enumerate(tracks_to_remove):
-        print idx_format % (idx + 1) + ' Removing ' + message
-        library.delete(track)
+        print idx_format % (idx + 1) + ' Remove ' + message
 
-    print 'Adding %s new files' % len(files_to_add)
+    print '%s new files to add' % len(files_to_add)
     idx_format = '%%%ss/%s' % (len(str(len(files_to_add))), str(len(files_to_add)))
     for idx, path in enumerate(files_to_add):
-        if not new_playlist:
-            print 'Creating playlist %s' % repr(new_playlist_name)
-            new_playlist = itunes.make(new=appscript.k.user_playlist, with_properties={appscript.k.name: new_playlist_name})
+        print idx_format % (idx + 1) + ' Add %s' % repr(path)
 
-        print idx_format % (idx + 1) + ' Adding %s' % repr(path)
-        library.add(mactypes.File(path).hfspath, to=new_playlist)
+    if len(tracks_to_remove) or len(files_to_add):
+        # Ask for permission
+        print 'Press Enter to continue or Ctrl-C to quit.'
+        sys.stdin.readline()
 
+        # Run tasks
+        print 'Removing %s tracks' % len(tracks_to_remove)
+        idx_format = '%%%ss/%s' % (len(str(len(tracks_to_remove))), str(len(tracks_to_remove)))
+        for idx, (track, message) in enumerate(tracks_to_remove):
+            print idx_format % (idx + 1) + ' Removing ' + message
+            library.delete(track)
+
+        print 'Adding %s new files' % len(files_to_add)
+        idx_format = '%%%ss/%s' % (len(str(len(files_to_add))), str(len(files_to_add)))
+        for idx, path in enumerate(files_to_add):
+            if not new_playlist:
+                print 'Creating playlist %s' % repr(new_playlist_name)
+                new_playlist = itunes.make(new=appscript.k.user_playlist, with_properties={appscript.k.name: new_playlist_name})
+
+            print idx_format % (idx + 1) + ' Adding %s' % repr(path)
+            library.add(mactypes.File(path).hfspath, to=new_playlist)
+
+if __name__ == '__main__':
+    main()
