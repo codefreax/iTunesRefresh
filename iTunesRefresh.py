7d4d687906c8a7b12ee1ee6d894c6184d6c24bf3
Thomas Friebel <yaron@codefreax.com>
2014-02-14 19:48:40 +0100

    Ignore the Movies directory

--- iTunesRefresh.py
+++ iTunesRefresh.py
@@ -18,7 +18,8 @@ def walk_library(lib_path):
         return path.endswith('.itlp') # iTunes LPs
 
     def is_ignored_path(path):
-        return (   path.startswith(lib_path + '/' + 'Podcasts/')
+        return (   path.startswith(lib_path + '/' + 'Movies/')
+                or path.startswith(lib_path + '/' + 'Podcasts/')
                 or path.startswith(lib_path + '/' + 'Tones/')
                 or path.startswith(lib_path + '/' + 'Automatically Add to iTunes.localized'))
 
